9e7fc7ae24d367a7d01b6fdc97e58b77f75c5e67
ecoknows <jerico.villaraza19@gmail.com>
2021-04-12 13:49:27 +0800

    Change PIPENV to VENV

---
+++ TUPScheduling/theme/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+      removeDeprecatedGapUtilities: true,
+      purgeLayersByDefault: true,
+  },
+  purge: {
+      enabled: false, //true for production build
+      content: [
+          '../**/templates/*.html',
+          '../**/templates/**/*.html'
+      ]
+  },
+  theme: {
+  },
+  variants: {},
+  plugins: [],
+}
