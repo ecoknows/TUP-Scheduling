af465627f1308c4a22e73a11891e9d86d2b325fc
RansuAndorei <lancejuat26@gmail.com>
2021-04-16 10:30:05 +0800

    professor input

--- TUPScheduling/home/models.py
+++ TUPScheduling/home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 from wagtail.core.models import Page, Orderable
 from wagtail.admin.edit_handlers import (
@@ -8,10 +10,14 @@ from wagtail.admin.edit_handlers import (
     InlinePanel,
 )
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
-from modelcluster.fields import ParentalKey
-from modelcluster.models import ClusterableModel
 from wagtail.snippets.models import register_snippet
 from wagtail.search import index
+
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
+import datetime
+
 
 class SubjectsOrderable(Orderable):
     model = ParentalKey("home.Subjects", related_name="subject_parental_key")
@@ -33,23 +39,23 @@ class Subjects(ClusterableModel, index.Indexed):
         null=True,
         help_text='Ex. CS33'
     )
-    
+
     description = models.CharField(
         max_length=200,
         null=True,
     )
     units = models.IntegerField(default=1)
-    
+
     lab_or_lec = models.CharField(
         max_length=200,
         default='Laboratory',
-        choices=[('Laboratory','Laboratory'),('Lecture', 'Lecture')]
+        choices=[('Laboratory', 'Laboratory'), ('Lecture', 'Lecture')]
     )
 
     sem = models.CharField(
         max_length=200,
         default='First',
-        choices=[('First','First'),('Second', 'Second')]
+        choices=[('First', 'First'), ('Second', 'Second')]
     )
 
     hours = models.FloatField(default=1)
@@ -66,12 +72,10 @@ class Subjects(ClusterableModel, index.Indexed):
         FieldPanel('sem', widget=forms.RadioSelect),
         FieldPanel('hours'),
         MultiFieldPanel([
-            InlinePanel('subject_parental_key', label='Subject', min_num=0, max_num=4)
-        ], heading = 'Prerequisite')
+            InlinePanel('subject_parental_key',
+                        label='Subject', min_num=0, max_num=4)
+        ], heading='Prerequisite')
     ]
-
-
-
 
     def __str__(self):
         return self.subject_code
@@ -82,7 +86,8 @@ class Subjects(ClusterableModel, index.Indexed):
         ordering = [
             'subject_code'
         ]
-    
+
+
 @register_snippet
 class CourseCurriculum(ClusterableModel):
     course_name = models.CharField(
@@ -110,17 +115,112 @@ class CourseCurriculum(ClusterableModel):
             'course_name'
         ]
 
+
 @register_snippet
 class Students(models.Model):
     pass
 
+
 @register_snippet
-class Professors(models.Model):
-    pass
+class Professors(ClusterableModel, index.Indexed):
+    global start_time
+
+    def validate_start_time(value):
+        global start_time
+        start_time = value
+        if value < datetime.time(7, 00, 00):
+            raise ValidationError(
+                _('%(value)s is invalid'),
+                params={'value': value},
+            )
+
+    def validate_end_time(value):
+        print(start_time, value)
+        if value > datetime.time(19, 00, 00):
+            raise ValidationError(
+                _('%(value)s is invalid'),
+                params={'value': value},
+            )
+        elif value <= start_time:
+            raise ValidationError(
+                _('end time must be greater than start time'),
+            )
+
+    first_name = models.CharField(
+        max_length=300,
+        null=True,
+        help_text='Ex. John'
+    )
+    middle_name = models.CharField(
+        max_length=300,
+        null=True,
+        help_text='Ex. Michael'
+    )
+    last_name = models.CharField(
+        max_length=300,
+        null=True,
+        help_text='Ex. Doe'
+    )
+    preferred_start_time = models.TimeField(
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        help_text='At least 7:00',
+        blank=True,
+        validators=[validate_start_time],
+    )
+    preferred_end_time = models.TimeField(
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        help_text='At most 19:00',
+        blank=True,
+        validators=[validate_end_time],
+    )
+    status = models.CharField(
+        max_length=200,
+        default='Regular',
+        choices=[('Regular', 'Regular'), ('Part-time', 'Part-time')]
+    )
+
+    search_fields = [
+        index.SearchField('subject_code'),
+    ]
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel('first_name'),
+                FieldPanel('middle_name'),
+                FieldPanel('last_name'),
+            ],
+            heading="Full Name",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('preferred_start_time'),
+                FieldPanel('preferred_end_time'),
+            ],
+            heading='Preferred Time',
+        ),
+        FieldPanel('status', widget=forms.RadioSelect),
+    ]
+
+    def __str__(self):
+        return self.last_name
+
+    class Meta:
+        verbose_name = 'Professor'
+        verbose_name_plural = 'Professors'
+        ordering = [
+            'last_name'
+        ]
+
 
 class HomePage(Page):
     max_count = 1
     pass
+
 
 class LoginPage(Page):
     pass
--- TUPScheduling/home/wagtail_hooks.py
+++ TUPScheduling/home/wagtail_hooks.py
@@ -1,55 +1,65 @@
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
 from .models import (
-	Subjects,
-	CourseCurriculum,
-	Professors,
-	Students
+    Subjects,
+    CourseCurriculum,
+    Professors,
+    Students
 )
 
 from wagtail.core import hooks
 
+
 class SubjectsAdmin(ModelAdmin):
-	model = Subjects
-	menu_label = 'Subjects'
-	list_display = ('subject_code', 'description', 'units', 'lab_or_lec', 'sem', 'hours')
-	list_filter = ('units','lab_or_lec', 'sem', 'hours')
-	search_fields = ('subject_code', 'description', 'units', 'lab_or_lec', 'sem', 'hours')
+    model = Subjects
+    menu_label = 'Subjects'
+    list_display = ('subject_code', 'description',
+                    'units', 'lab_or_lec', 'sem', 'hours')
+    list_filter = ('units', 'lab_or_lec', 'sem', 'hours')
+    search_fields = ('subject_code', 'description',
+                     'units', 'lab_or_lec', 'sem', 'hours')
+
 
 class CourseCurriculumAdmin(ModelAdmin):
-	model = CourseCurriculum
-	menu_label = 'Course Curriculum'
-	list_display = ('course_name', 'course_date')
-	search_fields = ('course_name', 'course_date')
-	list_filter = ('course_date',)
+    model = CourseCurriculum
+    menu_label = 'Course Curriculum'
+    list_display = ('course_name', 'course_date')
+    search_fields = ('course_name', 'course_date')
+    list_filter = ('course_date',)
+
 
 class AdminGroup(ModelAdminGroup):
-	menu_label = 'Admin'
-	menu_icon = 'user'
-	menu_order = 100
-	items = (SubjectsAdmin, CourseCurriculumAdmin)
+    menu_label = 'Admin'
+    menu_icon = 'user'
+    menu_order = 100
+    items = (SubjectsAdmin, CourseCurriculumAdmin)
+
 
 modeladmin_register(AdminGroup)
 
+
 class StudentsAdmin(ModelAdmin):
-	model = Students
-	menu_label = 'Students'
-	# list_display = ('name', 'preferred_time', 'regular_or_part_time', 'department')
-	# list_filter = ('name',)
-	# search_fields = ('name', )
+    model = Students
+    menu_label = 'Students'
+    # list_display = ('name', 'preferred_time', 'regular_or_part_time', 'department')
+    # list_filter = ('name',)
+    # search_fields = ('name', )
+
 
 class ProfessorsAdmin(ModelAdmin):
-	model = Professors
-	menu_label = 'Professors'
-	# list_display = ('course_name', 'course_date')
-	# search_fields = ('course_name', 'course_date')
-	# list_filter = ('course_date',)
+    model = Professors
+    menu_label = 'Professors'
+    list_display = ('last_name', 'preferred_start_time', 'preferred_end_time',
+                    'status')
+    list_filter = ('status',)
+    search_fields = ('first_name', 'middle_name', 'last_name', )
+
 
 class UsersGroup(ModelAdminGroup):
-	menu_label = 'User'
-	menu_icon = 'user'
-	menu_order = 100
-	items = (StudentsAdmin, ProfessorsAdmin)
+    menu_label = 'User'
+    menu_icon = 'user'
+    menu_order = 100
+    items = (StudentsAdmin, ProfessorsAdmin)
+
 
 modeladmin_register(UsersGroup)
-
