d93d3c57cbf03e51c32a35968b54b1ce25b39151
ecoknows <jerico.villaraza@gmail.com>
2021-05-02 18:00:12 +0800

    Fixing draggable issue

--- TUPScheduling/static/js/draggable.js
+++ TUPScheduling/static/js/draggable.js
@@ -1,7 +1,7 @@
 // var tiles = document.getElementsByClassName('tile')
 // var section_tile = document.getElementsByClassName('section-tile');
 
-function draggable(draggable_name,tile_name, is_tiling){
+function draggable(draggable_name,tile_name,paper_container ,is_tiling){
   let tiles = document.getElementsByClassName(tile_name);
   class DraggableElement{
     constructor(draggbablePaper){
@@ -46,6 +46,7 @@ function draggable(draggable_name,tile_name, is_tiling){
       this.pos3 = e.clientX;
       this.pos4 = e.clientY;
       // set the element's new position:
+      this.draggbablePaper.style.position = 'absolute'
       this.draggbablePaper.style.top = (this.draggbablePaper.offsetTop - this.pos2) + "px";
       this.draggbablePaper.style.left = (this.draggbablePaper.offsetLeft - this.pos1) + "px";
     }
@@ -72,35 +73,67 @@ function draggable(draggable_name,tile_name, is_tiling){
           const tile = this.draggableInfo(elementBoundingBox);
   
           if(this.checkCollision(draggablePaper,tile)){
-            this.draggbablePaper.style.top = elementBoundingBox.top + 'px'
-            this.draggbablePaper.style.left = elementBoundingBox.left + 'px'
-            // this.draggbablePaper.starting_tile = tiles[i];
+            
+            let container = tiles[i].querySelector('.tile-container');
+            container.appendChild(this.draggbablePaper);
+            this.draggbablePaper.style.top = null
+            this.draggbablePaper.style.left = null
 
+            
             if(is_tiling){
+
+              //RESET STARTING TILE 
+              if(this.starting_title_index){
+                for(let i = 0; i < this.paper_hours*5; i+=5){
+                  if(tiles[this.starting_title_index+i]?.occupied)
+                    tiles[this.starting_title_index+i].occupied = null;
+                }
+                this.starting_title_index = -1;
+              }
+
+
               let x = 0;
               while(x < this.paper_hours*5){
                 tiles[i+x].occupied = true;
                 x+=5;
               }
+              this.starting_title_index = i; 
             }
   
-            this.starting_tile_index = i; 
             return;
           }
+          
    
         }
         i++;
       }
+      const paperSection = document.getElementById(paper_container);
+      const paperContainerBoundingBox = paperSection.getBoundingClientRect()
+      const paperContainer = this.draggableInfo(paperContainerBoundingBox);
+
+      if(this.checkCollision(draggablePaper,paperContainer)){
+        paperSection.appendChild(this.draggbablePaper);
+        this.draggbablePaper.style.top = null
+        this.draggbablePaper.style.left = null
+
+        if(this.starting_title_index && is_tiling){
+          for(let i = 0; i < this.paper_hours*5; i+=5){
+            if(tiles[this.starting_title_index+i]?.occupied)
+              tiles[this.starting_title_index+i].occupied = null;
+          }
+          this.starting_title_index = -1;
+        }
+        this.draggbablePaper.style.position = null
+
+        return;
+      }
+
       // NOT COLLIDE RESET ALL 
       this.draggbablePaper.style.left = this.placementPositionLeft; 
       this.draggbablePaper.style.top = this.placementPositionTop;
-      if(this.draggbablePaper.starting_tile && is_tiling){
-        for(let i = 0; i < this.paper_hours*5; i+=5){
-          tiles[this.starting_tile_index+i].occupied = null;
-        }
-        this.starting_tile_index = -1;
+      if(!this.starting_title_index  && is_tiling ){
+        this.draggbablePaper.style.position = null
       }
-  
   
   
     }
@@ -137,5 +170,5 @@ function draggable(draggable_name,tile_name, is_tiling){
 
 
 
-draggable('draggable-paper','tile', true);
-draggable('draggable-professor','section-tile',false);
+draggable('draggable-paper','tile','section-container' ,true);
+draggable('draggable-professor','section-tile','professor-container',false);
