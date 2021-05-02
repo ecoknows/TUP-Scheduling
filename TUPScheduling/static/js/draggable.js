6ebe8637d3cea27abedda656f1532675eca8086c
ecoknows <jerico.villaraza@gmail.com>
2021-05-03 00:39:27 +0800

    Final Update for today

--- TUPScheduling/static/js/draggable.js
+++ TUPScheduling/static/js/draggable.js
@@ -1,7 +1,7 @@
 // var tiles = document.getElementsByClassName('tile')
 // var section_tile = document.getElementsByClassName('section-tile');
 
-function draggable(draggable_name,tile_name,paper_container){
+function draggable(draggable_name,tile_name,paper_container, is_main_table){
   let tiles = document.getElementsByClassName(tile_name);
   class DraggableElement{
     constructor(draggbablePaper){
@@ -10,7 +10,7 @@ function draggable(draggable_name,tile_name,paper_container){
       this.pos3 = 0;
       this.pos4 = 0;
       this.paper_hours = 3;
-      this.starting_tile = -1;
+      this.starting_tile_index = null;
       this.draggbablePaper = draggbablePaper;
       this.placementPositionTop =  draggbablePaper.style.top;
       this.placementPositionLeft =  draggbablePaper.style.left;
@@ -37,6 +37,7 @@ function draggable(draggable_name,tile_name,paper_container){
       // call a function whenever the cursor moves:
       document.onmousemove = this.elementDrag.bind(this);
     }
+    
     elementDrag(e){
       e = e || window.event;
       e.preventDefault();
@@ -59,6 +60,43 @@ function draggable(draggable_name,tile_name,paper_container){
         top: element.y
       }
     }
+
+
+    resetStartingTile(){
+      //RESET STARTING TILE   
+      if(is_main_table){
+
+        if(this.starting_tile_index != null){
+          for(let i = 0; i < this.paper_hours*5; i+=5){
+            if(tiles[this.starting_tile_index+i]?.occupied)
+              tiles[this.starting_tile_index+i].occupied = null;
+          }
+          this.starting_tile_index = null;
+        }
+      }else{
+        if(this.starting_tile_index  != null){
+          tiles[this.starting_tile_index].occupied = null
+          this.starting_tile_index = null;
+        }
+      }
+
+    }
+    
+    occupyingLogic(index){
+      this.resetStartingTile();
+      if(is_main_table){
+
+        let x = 0;     
+        while(x < this.paper_hours*5){
+          tiles[index+x].occupied = true;
+          x+=5;
+        }  
+      }else{
+        tiles[index].occupied = true;
+      }
+
+      this.starting_tile_index = index; 
+    }
   
   
     collision(){
@@ -78,24 +116,11 @@ function draggable(draggable_name,tile_name,paper_container){
             container.appendChild(this.draggbablePaper);
             this.draggbablePaper.style.top = null
             this.draggbablePaper.style.left = null
-
-            
-            //RESET STARTING TILE   
-            if(this.starting_title_index){
-              for(let i = 0; i < this.paper_hours*5; i+=5){
-                if(tiles[this.starting_title_index+i]?.occupied)
-                  tiles[this.starting_title_index+i].occupied = null;
-              }
-              this.starting_title_index = -1;
+            if(this.draggbablePaper.querySelector('#professor-name')){
+              this.draggbablePaper.querySelector('#professor-name').className = null
             }
 
-
-            let x = 0;
-            while(x < this.paper_hours*5){
-              tiles[i+x].occupied = true;
-              x+=5;
-            }
-            this.starting_title_index = i; 
+            this.occupyingLogic(i);
   
             return;
           }
@@ -112,23 +137,20 @@ function draggable(draggable_name,tile_name,paper_container){
         paperSection.appendChild(this.draggbablePaper);
         this.draggbablePaper.style.top = null
         this.draggbablePaper.style.left = null
-
-        if(this.starting_title_index){
-          for(let i = 0; i < this.paper_hours*5; i+=5){
-            if(tiles[this.starting_title_index+i]?.occupied)
-              tiles[this.starting_title_index+i].occupied = null;
-          }
-          this.starting_title_index = -1;
+        if(this.draggbablePaper.querySelector('#professor-name') && !is_main_table ){
+          this.draggbablePaper.querySelector('#professor-name').className = 'hidden'
         }
+
         this.draggbablePaper.style.position = null
 
+        this.resetStartingTile();
         return;
       }
 
       // NOT COLLIDE RESET ALL 
       this.draggbablePaper.style.left = this.placementPositionLeft; 
       this.draggbablePaper.style.top = this.placementPositionTop;
-      if(!this.starting_title_index ){
+      if(this.starting_tile_index == null){
         this.draggbablePaper.style.position = null
       }
   
@@ -167,5 +189,5 @@ function draggable(draggable_name,tile_name,paper_container){
 
 
 
-draggable('draggable-paper','tile','section-container');
-draggable('draggable-professor','section-tile','professor-container');
+draggable('draggable-paper','tile','section-container', true);
+draggable('draggable-professor','section-tile','professor-container', false);
