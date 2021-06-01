1a427e53dc8098360e47e6aab47b29ef9659239d
ecoknows <jerico.villaraza19@gmail.com>
2021-06-01 14:10:13 +0800

    Draggable Fix

--- TUPScheduling/static/js/draggable.js
+++ TUPScheduling/static/js/draggable.js
@@ -168,5 +168,137 @@ function draggable(draggable_name,tile_name,paper_container){
 
 
 
-draggable('draggable-paper','tile','section-container');
+// draggable('draggable-paper','tile','section-container');
 // draggable('draggable-professor','section-tile','professor-container', false);
+
+
+
+let tiles = document.getElementsByClassName('tile');
+let section_container = document.getElementById('section-container');
+
+function section_onmousedown( draggableSectionPaper){
+  let pos1 = 0;
+  let pos2 = 0;
+  let pos3 = 0;
+  let pos4 = 0;
+  let event = window.event;
+  let paper_hours = 3;
+  let draggableSection = draggableSectionPaper.parentElement
+
+  event.preventDefault();
+  pos3 = event.clientX;
+  pos4 = event.clientY;
+  
+  if(!draggableSection.is_dragged){
+    draggableSection.style.top = draggableSection.offsetTop - section_container.scrollTop + 'px'
+    draggableSection.is_dragged = true
+  }
+
+  
+  // console.log((draggableSection.offsetTop - pos2) + "px");
+  const elementDrag = e =>{
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    // set the element's new position:
+    draggableSection.style.position = 'absolute'
+    // console.log(draggableOffsetTop, e.clientY,draggableSection.offsetTop-section_container.scrollTop + 10);
+    draggableSection.style.top = (draggableSection.offsetTop  - pos2) + "px";
+    draggableSection.style.left = (draggableSection.offsetLeft - pos1) + "px";
+  }
+
+  
+  
+  function occupyingLogic(index){
+    let x = 0;     
+    while(x < paper_hours*6){
+      tiles[index+x].occupied = true;
+      x+=6;
+    }
+  
+    draggableSection.tileAssigned = index; 
+  }
+
+  
+  function checkCollision(paper, box){
+  
+    if(
+      paper.top > box.bottom || 
+      paper.right < box.left || 
+      paper.bottom < box.top || 
+      paper.left > box.right
+    ){
+      return false;
+    }else{
+      return true;
+    }
+
+  }
+  
+  function convertDraggable(element){
+    return{
+      bottom : element.y + element.height,
+      left: element.x,
+      right: element.x + element.width,
+      top: element.y
+    }
+  }
+  
+  
+  function collision(){
+  
+    const convertedDragable = convertDraggable(draggableSection.querySelector('#origin').getBoundingClientRect());
+    
+    if (draggableSection.tileAssigned != null){ 
+      let x = 0;     
+      while(x < paper_hours*6){
+        tiles[draggableSection.tileAssigned+x].occupied = false;
+        x+=6;
+      } 
+    }
+    let i = 0;
+
+    while(i < tiles.length){
+      if(!tiles[i].occupied){
+        const elementBoundingBox = tiles[i].getBoundingClientRect()
+        const tile = convertDraggable(elementBoundingBox);
+
+        if(checkCollision(convertedDragable,tile)){
+          
+          let container = tiles[i].querySelector('.tile-container');
+          container.appendChild(draggableSection);
+          draggableSection.style.top = null
+          draggableSection.style.left = null
+
+          occupyingLogic(i);
+
+          return;
+        }
+        
+ 
+      }
+      i++;
+    }
+    
+    draggableSection.style.top = null
+    draggableSection.style.left = null
+
+
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    draggableSection.onmouseup = null;
+    draggableSection.onmousemove = null;
+    collision()
+  }
+
+  draggableSection.onmouseup = closeDragElement
+  draggableSection.onmousemove = elementDrag
+
+}
--- TUPScheduling/static/js/infinite-draggable.js
+++ TUPScheduling/static/js/infinite-draggable.js
@@ -1,196 +1,3 @@
-// var tiles = document.getElementsByClassName('tile')
-// var section_tile = document.getElementsByClassName('section-tile');
-
-function infinite_draggable(draggable_name, tile_name, paper_container) {
-    let tiles = document.getElementsByClassName(tile_name);
-    const paperSection = document.getElementById(paper_container);
-
-    class DraggableElement {
-        constructor(draggbablePaper) {
-            this.pos1 = 0;
-            this.pos2 = 0;
-            this.pos3 = 0;
-            this.pos4 = 0;
-            this.paper_hours = 3;
-            this.starting_tile_index = null;
-            this.draggbablePaper = draggbablePaper;
-            this.placementPositionTop = draggbablePaper.style.top;
-            this.placementPositionLeft = draggbablePaper.style.left;
-            // this.paperHead = draggbablePaper.querySelector('#paper-head')
-            this.clone_draggablePaper = null;
-            this.dragElement();
-        }
-
-        dragElement() {
-
-            this.clone_draggablePaper = null
-            this.clone_draggablePaper = this.draggbablePaper.cloneNode(true);
-            this.clone_draggablePaper.style.position = 'absolute'
-            paperSection.appendChild(this.clone_draggablePaper)
-            this.clone_draggablePaper.style.top = (this.draggbablePaper.offsetTop - this.pos2) + "px";
-            this.clone_draggablePaper.style.left = (this.draggbablePaper.offsetLeft - this.pos1) + "px";
-            this.clone_draggablePaper.onmousedown = this.eventMouseDown.bind(this);
-        }
-
-
-        eventMouseDown(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            this.pos3 = e.clientX;
-            this.pos4 = e.clientY;
-            document.onmouseup = this.closeDragElement.bind(this);
-            // call a function whenever the cursor moves:
-            document.onmousemove = this.elementDrag.bind(this);
-        }
-
-        elementDrag(e) {
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            this.pos1 = this.pos3 - e.clientX;
-            this.pos2 = this.pos4 - e.clientY;
-            this.pos3 = e.clientX;
-            this.pos4 = e.clientY;
-            // set the element's new position:
-            // this.draggbablePaper.style.position = 'absolute'
-
-            this.clone_draggablePaper.style.top = (this.clone_draggablePaper.offsetTop - this.pos2) + "px";
-            this.clone_draggablePaper.style.left = (this.clone_draggablePaper.offsetLeft - this.pos1) + "px";
-        }
-
-        draggableInfo(element) {
-            return {
-                bottom: element.y + element.height,
-                left: element.x,
-                right: element.x + element.width,
-                top: element.y
-            }
-        }
-
-
-        resetStartingTile() {
-
-            if (this.starting_tile_index != null) {
-                tiles[this.starting_tile_index].occupied = null
-                this.starting_tile_index = null;
-            }
-        }
-
-        occupyingLogic(index) {
-            this.resetStartingTile();
-            tiles[index].occupied = true;
-
-            this.starting_tile_index = index;
-        }
-
-
-        collision() {
-
-            const draggablePaper = this.draggableInfo(this.clone_draggablePaper.querySelector('#origin').getBoundingClientRect());
-
-            let i = 0;
-
-            while (i < tiles.length) {
-                if (!tiles[i].occupied) {
-                    const elementBoundingBox = tiles[i].getBoundingClientRect()
-                    const tile = this.draggableInfo(elementBoundingBox);
-
-                    if (this.checkCollision(draggablePaper, tile)) {
-
-                        let container = tiles[i].querySelector('.tile-container');
-                        container.appendChild(this.clone_draggablePaper);
-                        this.clone_draggablePaper.style.top = null
-                        this.clone_draggablePaper.style.left = null
-
-                        if (this.clone_draggablePaper.querySelector('#professor-name')) {
-                            this.clone_draggablePaper.querySelector('#professor-name').className = null
-                        }
-
-
-                        this.occupyingLogic(i);
-
-                        return;
-                    }
-
-
-                }
-                i++;
-            }
-
-            this.clone_draggablePaper.remove();
-            this.clone_draggablePaper = null
-            // const paperContainerBoundingBox = paperSection.getBoundingClientRect()
-            // const paperContainer = this.draggableInfo(paperContainerBoundingBox);
-
-            // if (this.checkCollision(draggablePaper, paperContainer)) {
-            //     paperSection.appendChild(this.clone_draggablePaper);
-            //     this.clone_draggablePaper.style.top = null
-            //     this.clone_draggablePaper.style.left = null
-
-            //     if (this.clone_draggablePaper.querySelector('#professor-name')) {
-            //         this.clone_draggablePaper.querySelector('#professor-name').className = 'hidden'
-            //     }
-
-            //     this.clone_draggablePaper.style.position = null
-
-            //     this.resetStartingTile();
-            //     return;
-            // }
-
-            // // NOT COLLIDE RESET ALL 
-            // this.clone_draggablePaper.style.left = this.placementPositionLeft;
-            // this.clone_draggablePaper.style.top = this.placementPositionTop;
-            // if (this.starting_tile_index == null) {
-            //     this.clone_draggablePaper.style.position = null
-            // }
-
-
-        }
-
-        checkCollision(paper, box) {
-
-            if (
-                paper.top > box.bottom ||
-                paper.right < box.left ||
-                paper.bottom < box.top ||
-                paper.left > box.right
-            ) {
-                return false;
-            } else {clone_draggablePaper
-                return true;
-            }
-
-        }
-
-        closeDragElement() {
-            document.onmouseup = null;
-            document.onmousemove = null;
-            this.collision();
-            this.dragElement();
-        }
-
-    }
-
-    var draggables = document.getElementsByClassName(draggable_name);
-
-    Array.prototype.forEach.call(draggables, (element) => {
-        new DraggableElement(element);
-    });
-}
-
-
-
-// infinite_draggable('draggable-professor', 'section-tile', 'professor-container');
-
-
-
-// var draggable_professors = document.getElementsByClassName('draggable-professor');
-
-// // Array.prototype.forEach.call(draggables, (element) => {
-// //     new DraggableElement(element);
-// // });
-
 
 let tiles_section = document.getElementsByClassName('section-tile');
 const professorContainer = document.getElementById('professor-container');
@@ -205,7 +12,7 @@ class DragProfessor{
         this.placementPositionLeft = draggableProfessor.style.left;
         this.draggableProfessor = draggableProfessor
         this.draggableProfessor.onmousedown = this.eventMouseDown.bind(this);
-        // this.draggableProfessor.onmouseout = this.eventMouseOut.bind(this);
+        this.tileAssigned = null;
     }
 
     eventMouseDown(e){
@@ -213,14 +20,9 @@ class DragProfessor{
         e.preventDefault();
         this.pos3 = e.clientX;
         this.pos4 = e.clientY;
+        this.draggableProfessor.style.position = 'absolute'
         document.onmouseup = this.closeDragElement.bind(this);
         document.onmousemove = this.elementDrag.bind(this);
-    }
-
-    eventMouseOut(e){
-        e = e || window.event;
-        e.preventDefault();
-        this.draggableProfessor.remove();
     }
 
 
@@ -246,24 +48,20 @@ class DragProfessor{
       }
     }
 
-    resetStartingTile() {
-        if (this.starting_tile_index != null) {
-            tiles_section[this.starting_tile_index].occupied = null
-            this.starting_tile_index = null;
-        }
-    }
-
     occupyingLogic(index) {
-        this.resetStartingTile();
         tiles_section[index].occupied = true;
-
-        this.starting_tile_index = index;
+        this.tileAssigned = index;
     }
 
     
     collision(){
         const draggablePaper = this.convertDraggable(this.draggableProfessor.querySelector('#origin').getBoundingClientRect());
-      
+        this.draggableProfessor.style.position = null
+        
+        if (this.tileAssigned != null){ 
+            tiles_section[this.tileAssigned].occupied = false;
+        }
+        
         let i = 0;
 
         while (i < tiles_section.length) {
@@ -282,7 +80,6 @@ class DragProfessor{
                         this.draggableProfessor.querySelector('#professor-name').className = null
                     }
 
-
                     this.occupyingLogic(i);
 
                     return;
@@ -293,15 +90,7 @@ class DragProfessor{
             i++;
         }
 
-        // this.draggableProfessor.remove();
-        // this.draggableProfessor = null
-        // console.log('DASFSA');
-
-        this.draggableProfessor.style.left = null;
-        this.draggableProfessor.style.top = null;
-        // if (this.starting_tile_index == null) {
-        //     this.draggableProfessor.style.position = null
-        // }
+        this.draggableProfessor.remove()
         
     }
 
@@ -326,7 +115,6 @@ class DragProfessor{
     closeDragElement(){
       document.onmouseup = null;
       document.onmousemove = null;
-    //   this.draggableProfessor.remove();
       this.collision();
     }
     
@@ -340,6 +128,5 @@ function professor_onmousemove(draggableProfessor){
     cloneDraggableProfessor.style.position = 'absolute'
     cloneDraggableProfessor.style.top = draggableProfessor.offsetTop + 'px'
     professorContainer.appendChild(cloneDraggableProfessor)
-
     new DragProfessor(cloneDraggableProfessor)
 }
