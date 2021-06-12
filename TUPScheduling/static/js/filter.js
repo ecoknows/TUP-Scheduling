fe327a0659a4748b15b6ba0a91c2ab8624e12e58
RansuAndorei <lancejuat26@gmail.com>
2021-06-12 14:29:18 +0800

    filter

--- TUPScheduling/static/js/filter.js
+++ TUPScheduling/static/js/filter.js
@@ -18,3 +18,32 @@ function filter(){
     }
     //console.log(filterables[0].children[0].children[0].innerText)
 }
+
+function filterProf(){
+    text = document.getElementById('professor-filter').value;
+    filterables = document.getElementsByClassName('filter-prof');
+
+    if(text.localeCompare('') == 0){
+        for(let i = 0; i < filterables.length; i++){
+            filterables[i].style.display = 'block';
+        }
+    }else{
+        for(let i = 0; i < filterables.length; i++){
+            if(filterables[i].children[0].children[0].innerText.toUpperCase().indexOf(text.toUpperCase()) != -1 ){
+                filterables[i].style.display = 'block';
+            }else{
+                let flag = 0;
+                for(let j = 0; j < filterables[i].children[1].children.length; j++){
+                    if(filterables[0].children[1].children[j].children[0].children[0].children[0].children[1].innerText.toUpperCase().indexOf(text.toUpperCase()) != -1 ){
+                        filterables[i].style.display = 'block';
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 0){
+                    filterables[i].style.display = 'none';
+                }
+            }
+        }
+    }
+}
